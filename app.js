e2d6774d2c0736dbbb031f0757eed5f97ff52dc7
Ronan <galv.ronan@gmail.com>
2020-12-16 21:02:27 -0500

    inquirer npm imported

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+//            const fs = require('fs');
+//            const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+//            const pageHTML = generatePage(name, github);
+
+// process.argv replaced by inquirer npm
+//const profileDataArgs = process.argv.slice(2);
 /* Same as
 const profileDataArgs = process.argv.slice(2, process.argv.length); */
 
@@ -9,19 +13,23 @@ const profileDataArgs = process.argv.slice(2, process.argv.length); */
 const name = profileDataArgs[0];
 const github = profileDataArgs[1];
 Same as below */
-const [name, github] = profileDataArgs;
+// this is part of process.argv so it's removed as well
+//const [name, github] = profileDataArgs;
 
-// generatePage() was here
+// generatePage() was here before being exported
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
-/* Same as
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err; */
+//            fs.writeFile('./index.html', pageHTML, err => {
+//                if (err) throw err;
+
+// this was replaced b/c of generatePage(name, github)
+/* fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err); */
+/* also,
+    if (err) throw new Error (err); is the same as one used above */
 
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//                console.log('Portfolio complete! Check out index.html to see the output!');
+//            });
 
 /*
 // Notice the lack of parentheses around the 'profileDataArr' parameter?
@@ -39,3 +47,13 @@ const printProfileData = profileDataArr => {
 
 printProfileData(profileDataArgs);
 */
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
